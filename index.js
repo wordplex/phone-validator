7450eaabf650c39df6fa2cb65a1493111dd7d30e
mohamedWagdy <wagdymohamedd@gmail.com>
2021-10-22 19:21:12 +0200

    Added switch toggle to enable and disable countryCode

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
 import { countries } from './countries.js';
 
-const input = document.getElementById("input")
-const selector = document.getElementById("select")
-const button = document.getElementById("button")
+const input = document.getElementById("input");
+const selector = document.getElementById("select");
+const button = document.getElementById("button");
+const countryCodeSwitcher = document.getElementById("switcher");
 
-input.addEventListener('change',inputData)
-button.addEventListener('click' , displayData)
 
 function inputData() {
-  const phoneNumber = parseInt (input.value, 10);
+  const phoneNumber = parseInt(input.value, 10);
   
-  if (typeof phoneNumber ==='number') {
-   
+  if (typeof phoneNumber === 'number') {
     return phoneNumber
-
   }else {
-    console.log ('please enter valid number')
+    console.log ('please enter valid number');
   }
 }
 
 async function displayData() {
-  const PhoneNumbers = inputData()
-  const countryNumber = selector.value
-  console.log(countryNumber)
-  fetch(`http://geo.wordplex.io/v4/phone?phone=${PhoneNumbers}&country=${countryNumber}`)
-    .then((res) => res.json ())
+  const phoneNumbers = inputData();
+  const countryNumber = selector.value;
+  fetch(`http://geo.wordplex.io/v4/phone?phone=${phoneNumbers}&codes=${countryNumber}`)
+    .then((res) => res.json())
     .then((data) => {
-    console.log (data)
-  }).catch(err => {
-    console.log (err)
-  })
+    console.log (data);
+    })
+    .catch((err) => {
+      console.log (err);
+    })
 }
 
-function countrySelector() {
-  countries.forEach(element =>{
-    const option = document.createElement ('option')
-    option.innerHTML= element.name
-    option.value = element.code
-    select.append (option)
+(function countrySelector() {
+  countries.forEach(element => {
+    const option = document.createElement ('option');
+
+    option.innerHTML = `${element.name} (${element.code})`;
+    option.value = element.code;
+
+    selector.append(option);
   });
+})()
+
+
+function countrySwitcherHandler(event) {
+  selector.disabled = !(!!event.target.checked);
 }
- 
-countrySelector();
+
+input.addEventListener('change', inputData);
+button.addEventListener('click', displayData);
+countryCodeSwitcher.addEventListener('change', countrySwitcherHandler);
